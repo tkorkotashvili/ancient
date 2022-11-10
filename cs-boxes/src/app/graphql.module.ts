73071c935e8fce88613e2f6286de70b0602adf87
temo.korkotashvili <temo.korkotashvili@gmail.com>
2022-11-10 09:12:06 +0400

    add apollo-link ws and refactor

--- cs-boxes/src/app/graphql.module.ts
+++ cs-boxes/src/app/graphql.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { OperationDefinitionNode } from 'graphql';
 
 const uri = 'https://api-staging.csgoroll.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const ws = new WebSocketLink({
+    uri: `ws://api-staging.csgoroll.com/graphql`,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  const http = httpLink.create({ uri, withCredentials: true });
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = <OperationDefinitionNode>(
+        getMainDefinition(query)
+      );
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    ws,
+    http
+  );
   return {
-    link: httpLink.create({ uri, withCredentials: true }),
+    link: link,
     cache: new InMemoryCache(),
   };
 }
